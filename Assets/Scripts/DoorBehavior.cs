b052513c79cde3905974116b819107007576f32f
s3ns3i <eragonsm@gmail.com>
2015-12-01 21:47:31 +0100

    There's a map, a flashlight and door mechanics

---
+++ Assets/Scripts/DoorBehavior.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorBehavior : MonoBehaviour {
+
+	bool doorClosed = true;
+	[SerializeField]
+	float rayLength = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+
+		if (Physics.Raycast(transform.position, forward, out hit, rayLength) || Physics.Raycast(transform.position, -forward, out hit, rayLength))
+		{
+			if (hit.collider.gameObject.tag == "Door")
+			{
+				if (Input.GetButtonDown("Use"))
+				{
+					Animator doorAnimator = hit.collider.gameObject.transform.parent.GetComponent<Animator>();
+					doorAnimator.SetTrigger("door_action");
+					doorClosed = !doorClosed;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public bool isCursorLocked = true;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("escape") && isCursorLocked)
+        {
+            isCursorLocked = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else if (Input.GetButtonDown("Fire1") && !isCursorLocked)
+        {
+            isCursorLocked = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+	}
+}
